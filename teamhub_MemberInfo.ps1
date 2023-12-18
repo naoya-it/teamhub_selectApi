c107c53960b480b78753b61b3eb3d4e1b8c1c496
Naoya_HNZW <54469609+naoya-it@users.noreply.github.com>
2023-12-18 21:48:42 +0900

    Add files via upload

---
+++ teamhub_MemberInfo.ps1
@@ -0,0 +1,42 @@
+﻿
+cls
+
+##後程config持ちにする
+#ログインURL --ログインセッション確立の為に必須
+$loginUrl="https://api.tmhub.jp/1/login"  
+
+#ログイン時情報
+$loginParams = @{
+email="自分のメールアドレス"
+password="パスワード"
+}
+
+#チームID
+$teamId="xxxxx"
+
+#メンバー情報のリスト
+$reqURL="https://api.tmhub.jp/1/teams/${teamId}/teamMembersAndGuestsByTypes"
+
+#ログインセッション確立 必須
+$ls=Invoke-WebRequest $loginUrl -body $loginParams -SessionVariable loginSession
+
+#WebRequest
+$req=Invoke-WebRequest -Method Get $reqURL -WebSession $loginSession
+
+#変数定義
+$members=$null
+
+#contentのJsonに変換⇒Jsonから変換
+#文字化けしないし、コンソールとかからは見やすい
+#$req.Content | ConvertFrom-Json | ConvertTo-Json
+
+#メンバー情報を格納
+#NotePropertyでごにょごにょしているのは、MemberTypeをNotePropertyで絞り
+#SysytemPropertyの取得を防ぐため。
+$reqJson=$req.Content | ConvertFrom-Json
+$memberType=$reqJson | get-member | Where-Object {$_.MemberType -eq "NoteProperty"}
+foreach ( $mType in $MemberType ) { $members += $reqJson.($mType.Name) }
+
+
+#出力 後で情報をファイルにアウトプットする
+$members
---
+++ teamhub_eventInfo.ps1
@@ -0,0 +1,40 @@
+﻿cls
+
+##後程config持ちにする
+#ログインURL --ログインセッション確立の為に必須
+$loginUrl="https://api.tmhub.jp/1/login"  
+
+#ログイン時情報
+$loginParams = @{
+email="自分のメールアドレス"
+password="パスワード"
+}
+
+#チームID
+$teamId="xxxxx"
+
+#イベント情報 日付 start
+$dateStart="2023-12-01T15:00:00.000Z"
+#イベント情報 日付 end
+$dateEnd="2023-12-25T14:59:59.999Z"
+#イベント情報 抽出上限
+$selectLimit="999"
+
+#イベント情報の取得
+$reqURL="https://api.tmhub.jp/1/teams/${teanId}/events?from=${dateStart}&to=${dateEnd}&limit=${selectLimit}"
+
+#ログインセッション確立 必須
+$ls=Invoke-WebRequest $loginUrl -body $loginParams -SessionVariable loginSession
+
+#WebRequest
+$req=Invoke-WebRequest -Method Get $reqURL -WebSession $loginSession
+
+#contentのJsonに変換⇒Jsonから変換
+#文字化けしないし、コンソールとかからは見やすい
+#$req.Content | ConvertFrom-Json | ConvertTo-Json
+
+$eventJson=$req.Content | ConvertFrom-Json
+
+#イベント情報
+$eventJson 
+
